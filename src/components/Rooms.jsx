6f34fbb77ee86ff401da1f02f162c59d24b96ee5
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-31 12:13:53 +0300

    rooms block add 3 photo to project

--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import rooms1 from '../assets/rooms1.png'
+import rooms2 from '../assets/rooms2.png'
+import rooms3 from '../assets/rooms3.png'
 
 const Rooms = () => {
   return (
     <div>
+      <div>
+        <h3>Fine Interior Rooms</h3>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur impedit libero non, saepe veniam
+          voluptatem?
+        </p>
+      </div>
 
+      <div>
+        <img src={rooms1} alt="/"/>
+        <img src={rooms2} alt="/"/>
+        <img src={rooms3} alt="/"/>
+      </div>
     </div>
   )
 }
