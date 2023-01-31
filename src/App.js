7f90ee8d810535ef5b090989faec6572f02da207
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-31 12:20:42 +0300

    imageslider block add to project

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Offers from "./components/Offers";
 import Plan from "./components/Plan";
 import Rooms from "./components/Rooms";
+import ImageSlider from "./components/ImageSlider";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Offers/>
       <Plan/>
       <Rooms/>
+      <ImageSlider/>
     </div>
   );
 }
--- src/components/Rooms.jsx
+++ src/components/Rooms.jsx
@@ -5,19 +5,20 @@ import rooms3 from '../assets/rooms3.png'
 
 const Rooms = () => {
   return (
-    <div>
-      <div>
-        <h3>Fine Interior Rooms</h3>
-        <p>
+    <div
+      className='max-w-[1400px] h-[500px] bg-blue-100 mx-auto my-20 pt-16 lg:mb-[20%] md:mb-[35%] px-4 grid lg:grid-cols-3 gap-4'>
+      <div className='lg:top-20 relative lg:col-span-1 col-span-2'>
+        <h3 className='text-2xl font-bold'>Fine Interior Rooms</h3>
+        <p className='pt-4'>
           Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur impedit libero non, saepe veniam
           voluptatem?
         </p>
       </div>
 
-      <div>
-        <img src={rooms1} alt="/"/>
-        <img src={rooms2} alt="/"/>
-        <img src={rooms3} alt="/"/>
+      <div className='grid grid-cols-2 col-span-2 gap-2'>
+        <img className='object-cover w-full h-full' src={rooms1} alt="/"/>
+        <img className='row-span-2 object-cover w-full h-full' src={rooms2} alt="/"/>
+        <img className='object-cover w-full h-full' src={rooms3} alt="/"/>
       </div>
     </div>
   )
