e2a17f3cc8512b5e86309ae3c1aec00de5a0888d
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-31 13:05:12 +0300

    footer block add icon to project

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {FaFacebook, FaInstagram, FaTwitter} from "react-icons/fa";
+import {TiSocialPinterest} from "react-icons/ti";
 
 const Footer = () => {
   return (
