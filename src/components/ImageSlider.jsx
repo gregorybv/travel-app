fdfc0f110196582c9f151bf8000c0bb6904b3278
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-31 12:45:21 +0300

    imageslider block add icon to project

--- src/components/ImageSlider.jsx
+++ src/components/ImageSlider.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {BsChevronCompactLeft, BsChevronCompactRight} from "react-icons/bs";
 
 const ImageSlider = () => {
 
@@ -21,8 +22,19 @@ const ImageSlider = () => {
     },
   ]
   return (
-    <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative'>
-      <div className='w-full h-full rounded-2xl bg-center bg-cover duration-500' style={{backgroundImage: `url(${slides[0].url})`}}></div>
+    <div className='max-w-[1400px] h-[580px] w-full m-auto py-16 px-4 relative group'>
+      <div
+        className='w-full h-full rounded-2xl bg-center bg-cover duration-500'
+        style={{backgroundImage: `url(${slides[0].url})`}}>
+      </div>
+      <div className='absolute top-[50%] -translate-x-0 -translate-y-[-50%] left-5
+      text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactLeft size={30}/>
+      </div>
+      <div className='absolute top-[50%] -translate-x-0 -translate-y-[-50%] right-5
+      text-2xl rounded-full p-2 group-hover:bg-black/20 text-white cursor-pointer'>
+        <BsChevronCompactRight size={30}/>
+      </div>
     </div>
   )
 }
