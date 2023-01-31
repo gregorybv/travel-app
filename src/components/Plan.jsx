b39c3e281f265b785c0736c013f11a1286a44dfe
Grogoiy Berezin <gregory_berezin@outlook.com>
2023-01-31 11:39:27 +0300

    plan block add 4 photo to project

--- src/components/Plan.jsx
+++ src/components/Plan.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import photo1 from 'https://unsplash.com/photos/Bkci_8qcdvQ'
 
 const Plan = () => {
   return (
     <div>
-
+      <div>
+        <img src={photo1} alt="unsplash"/>
+      </div>
     </div>
   )
 }
